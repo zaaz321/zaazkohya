721de03f7cd753ac9ce732cdf8973498156e3fdc
zaaz321 <133797470+zaaz321@users.noreply.github.com>
2023-06-04 20:31:55 +0200

    Create runpod_kohya2.sh

---
+++ runpod_kohya2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+git clone https://github.com/bmaltais/kohya_ss.git
+
+cd kohya_ss
+
+git checkout 5ebc697b1e485c1713dbc7d29142848b2b158ccb
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+./setup.sh -n
+
+./setup.sh -n
+
+apt update
+
+apt-get install python3.10-tk
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+yes | python3.10 -m pip uninstall xformers
+
+yes | python3.10 -m pip install xformers
+
+yes | pip3 uninstall torch torchvision torchaudio
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
